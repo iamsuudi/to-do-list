3ffe3e7875394c64ae7e48ac26a75396d628d4e8
abdulfetahsuudi <144704715+abdulfetahsuudi@users.noreply.github.com>
2024-01-08 10:38:32 +0300

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: {
+            import: './src/index.js',
+        },
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/i,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.styl$/i,
+                use: ['style-loader', 'css-loader', 'stylus-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(css)$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|gif|jpg|jepg|svg|woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            }
+        }
+    }
+}
