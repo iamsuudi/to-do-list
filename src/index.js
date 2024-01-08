84a8c196d512bd45a93e7a9f876da0ed5e037024
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-01-08 23:03:33 +0300

    Create a function which renders the todo members

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ createToDo('personal', 'pray your selat', 'tomorrow', 'high');
 createToDo('personal', 'do exercise', 'tomorrow', 'high');
 createToDo('personal', 'create some notes', 'tomorrow', 'high');
 
-displayToDos();
+const todos = displayToDos();
+
+function render() {
+    const divList = document.querySelector('div.list');
+
+    todos.forEach(td => {
+        const p = document.createElement('p');
+        p.textContent = td.getDescription();
+        divList.appendChild(p);
+    });
+}
+
+render();
