6173b3834575c64383ba783599f05e1b0a260d3e
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-01-15 23:41:31 +0300

    Add default todos to each default projects

--- src/project.js
+++ src/project.js
@@ -33,4 +33,23 @@ function displayAllToDos() {
     return projects.map(proj => todos[proj]).flat();
 }
 
+
+// add default todos to personal category
+createToDo('personal', 'Do the laundary', 'today', 'high');
+createToDo('personal', 'Pray your selat', 'today', 'high');
+createToDo('personal', 'Do exercise', 'today', 'high');
+createToDo('personal', 'Create some notes', 'tomorrow', 'high');
+
+// add default todos to work category
+createToDo('work', 'Do the assignment', 'tonight', 'high');
+createToDo('work', 'Submit your proposal', 'wednesday', 'high');
+createToDo('work', 'Write your CV', 'sunday', 'high');
+createToDo('work', 'Connect with 10 people', 'today', 'high');
+
+// add default todos to grocery category
+createToDo('grocery', 'Go to grocery 1 and buy avocado', 'tomorrow', 'high');
+createToDo('grocery', 'Buy salad', 'today', 'high');
+createToDo('grocery', 'Buy some milk', 'tomorrow', 'high');
+
+
 export {createToDo, displayAllToDos, displayProjectToDos, addProject, projectTitles};
