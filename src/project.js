a1787067a33355cab39357e9e1912a9feb721713
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-01-20 17:17:26 +0300

    Add localStorage to the app
    * It actually saves each todo object's fields in an array which will be stored,
    in place of todo object.
    * Which will then be synced to localStorage.
    * loadFromLocalStorage() is always called on   page refresh or first page opening,
    to load synced todos' six fields.
    Which then creates todo object back from the fields and stores them in todos container.
    * When loadFromLocalStorage() finishes loading the pre-availabe todos,
    it proceeds to call sync() every 1 second to sync the changes made.

--- src/project.js
+++ src/project.js
@@ -6,9 +6,47 @@ const todos = {
     grocery: [],
 };
 
-const projects = ['personal', 'work', 'grocery'];
+let local = {};
+
+const projects = [];
+
+function sync() {
+    
+    for (let i = 0; i < projects.length; i += 1) {
+        
+        const td = todos[projects[i]];
+        // console.log(td);
+        local[projects[i]] = [];
+
+        for (let j = 0; j < td.length; j += 1) {
+
+            const title = td[j].getTitle();
+            const description = td[j].getDescription();
+            const dueDate = td[j].getDueDate();
+            const priority = td[j].getPriority();
+            const note = td[j].getNote();
+            const status = td[j].getStatus();
+    
+            local[projects[i]][j] = [title, description, dueDate, priority, note, status];
+        }
+    }
+
+    localStorage.setItem("local", JSON.stringify(local));
+}
+
+function addProject(title) {
+    projects.push(title);
+    todos[title] = [];
+
+    // add the project to localStorge
+    localStorage.setItem("projects", JSON.stringify(projects));
+}
 
 function createToDo(title, description, dueDate, priority) {
+
+    if (!(title in todos))
+        addProject(title);
+
     // create to do object
     const todo = new ToDo(title, description, dueDate, priority);
 
@@ -18,11 +56,6 @@ function createToDo(title, description, dueDate, priority) {
 
 function displayProjectToDos(title) {
     return todos[title];
-}
-
-function addProject(title) {
-    projects.push(title);
-    todos[title] = [];
 }
 
 function getProjectTitles() {
@@ -37,6 +70,9 @@ function deleteProject(title) {
     delete todos[title];
     const index = projects.indexOf(title);
     projects.splice(index, 1);
+
+    // update projects list in localStorage
+    window.localStorage.setItem("projects", JSON.stringify(projects));
 }
 
 function getLessThanFrequency(title) {
@@ -58,7 +94,45 @@ function deleteTodo(title, todoIndex, arrayType) {
         todos[title].splice(todoIndex, 1);
 }
 
-// add default todos to personal category
+function loadFromLocalStorage() {
+    const newTitlesList = JSON.parse(localStorage.getItem("projects"));
+
+    // load project titles to the projects list
+    if (!newTitlesList);
+    else
+        projects.splice(0, projects.length, ...newTitlesList);
+
+    // load all todos in each project to their respective array in the todos object
+    const syncedLocal = JSON.parse(localStorage.getItem("local"));
+    
+    if ( syncedLocal && Object.keys(syncedLocal).length !== 0 ) {
+        local = syncedLocal;
+        for (let i = 0; i < projects.length; i += 1) {
+
+            if (!todos[projects[i]])
+                todos[projects[i]] = [];
+            
+            // create to do object
+            const core = local[projects[i]];
+
+            for (let j = 0; j < core.length; j += 1) {
+                const param = core[j].slice(0, 4);
+                const todo = new ToDo(...param);
+                todo.setNote(core[j][4]);
+                todo.setStatus(core[j][5]);
+                todos[projects[i]].push(todo);
+            }
+        }
+    }
+
+    // sync the todos every one second
+    setInterval(sync, 1000);
+}
+
+// addDefaultsToLocalStorage();
+loadFromLocalStorage();
+
+/* // add default todos to personal category
 createToDo('personal', 'Do the laundary', 'today', 'high');
 createToDo('personal', 'Pray your selat', 'today', 'high');
 createToDo('personal', 'Do exercise', 'today', 'high');
@@ -73,7 +147,7 @@ createToDo('work', 'Connect with 10 people', 'today', 'high');
 // add default todos to grocery category
 createToDo('grocery', 'Go to grocery 1 and buy avocado', 'tomorrow', 'high');
 createToDo('grocery', 'Buy salad', 'today', 'high');
-createToDo('grocery', 'Buy some milk', 'tomorrow', 'high');
+createToDo('grocery', 'Buy some milk', 'tomorrow', 'high'); */
 
 
 export {createToDo, displayAllToDos, displayProjectToDos, addProject, getProjectTitles, deleteProject, deleteTodo};
