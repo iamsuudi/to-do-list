b5570989c68efc6e1b10d930a2c58af274f4a415
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-01-27 16:08:59 +0300

    Create priority-picker class

---
+++ src/priority-picker.js
@@ -0,0 +1,78 @@
+
+
+export default class Priority {
+
+
+    divPriorities = document.querySelector('dialog div.priorities');
+
+    constructor(todos, index, priority, clickedTodo) {
+
+        this.todos = todos;
+        this.index = index;
+        this.priority = priority;
+        this.clickedTodo = clickedTodo;
+
+        // render the current todos priority
+        this.renderCurrentTodoPriority();
+
+        // make the priority panel visible
+        this.divPriorities.classList.add('visible');
+
+        // add listener to the buttons
+        this.listenToBtns();
+    }
+
+    renderCurrentTodoPriority() {
+
+        this.divPriorities.querySelectorAll('button').forEach(btn => {
+
+            btn.classList.remove('selected');
+
+            if (btn.classList[0] === this.todos[this.index].getPriority())
+                btn.classList.add('selected');
+        });
+    }
+
+    btnClicked(clickedBtn) {
+
+        if (clickedBtn.className === 'cancel')
+            setTimeout(() => {
+                this.divPriorities.classList.remove('visible');
+            }, 200);
+        else {
+
+            this.switchCurrentSelectedPriorityBtn(clickedBtn);
+
+            // sync the change to the todo object
+            this.todos[this.index].setPriority(clickedBtn.className);
+
+            // remove todo if it's in a different priority category
+            if (this.priority !== 'all-todos')
+                this.clickedTodo.parentElement.remove();
+        }
+    }
+
+    listenToBtns() {
+        
+        const allPriorityBtns = this.divPriorities.querySelectorAll('button');
+
+        allPriorityBtns.forEach(btn => {
+
+            btn.addEventListener('click', event => {
+
+                this.btnClicked(event.target);
+
+            });
+        })
+    }
+
+    switchCurrentSelectedPriorityBtn(clickedBtn) {
+
+        const currentSelctedPriority = this.divPriorities.querySelector('button.selected');
+
+        if (currentSelctedPriority)
+            currentSelctedPriority.classList.remove('selected');
+
+        clickedBtn.classList.add('selected');
+    }
+}
