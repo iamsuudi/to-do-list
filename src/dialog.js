c5b1b666888f0d45b75d38424210516d8ed4df7a
abdulfetasuudi <suudiabdulfetah@gmail.com>
2024-02-08 21:53:07 +0300

    Remove dataindexFilter()
    It was used to adjust dataindex of todo nodes,
    now we have id to keep track of them.

--- src/dialog.js
+++ src/dialog.js
@@ -1,24 +1,21 @@
-import {deleteTodo} from "./project";
+import { deleteTodo } from './project';
 
 export default class Dialog {
-
     dialog = document.querySelector('dialog.detail');
 
     divPriorities = document.querySelector('dialog div.priorities');
 
+    divDatePicker = document.querySelector('div.date-picker');
 
-
-    constructor(todoObj, title, index, priority, clickedTodo) {
-
+    constructor(todoObj, title, index, clickedTodo) {
         this.todoObj = todoObj;
         this.title = title;
         this.index = index;
-        this.priority = priority;
         this.clickedTodo = clickedTodo;
-    console.log(this.todoObj);
-    console.log(this.title);
-    console.log(this.priority);
-    console.log(this.clickedTodo);
+        console.log(this.todoObj);
+        console.log(this.title);
+        console.log(this.priority);
+        console.log(this.clickedTodo);
 
         // update the title on dialog header
         const spanTitle = this.dialog.querySelector('span.title');
@@ -26,7 +23,7 @@ export default class Dialog {
 
         // display Description and Note of the todo
         this.renderDescriptionAndNote();
-        
+
         this.dialog.showModal();
 
         // add listener to date picker
@@ -35,20 +32,19 @@ export default class Dialog {
             this.displayDatePicker();
         });
 
-
         // add listener to changing-priority button
-        const changePriorityBtn = document.querySelector('button.change-priority');
+        const changePriorityBtn = document.querySelector(
+            'button.change-priority'
+        );
         changePriorityBtn.addEventListener('click', () => {
             this.displaypriorityPanel();
         });
-
 
         // add listener to delete button of todos
         const deleteTodoBtn = document.querySelector('dialog button.delete');
         deleteTodoBtn.addEventListener('click', () => {
             this.deleteTodoClicked();
         });
-
 
         // add listener to cancel button of dialog
         const cancelDialogBtn = document.querySelector('dialog button.cancel');
@@ -58,92 +54,71 @@ export default class Dialog {
     }
 
     renderDescriptionAndNote() {
-
         const description = this.dialog.querySelector('input.todo-description');
         const note = this.dialog.querySelector('textarea#note');
 
         description.value = this.todoObj.getDescription();
         note.value = this.todoObj.getNote();
-    
+
         // update and sync description and note dynamically on change
         this.SyncDescriptionAndNoteDynamically(description, note);
     }
 
     SyncDescriptionAndNoteDynamically(description, note) {
-
         // listen to change and update dynamically
-        description.addEventListener('input', e => {
-    
+        description.addEventListener('input', (e) => {
             // sync to the todo object
             this.todoObj.setDescription(e.target.value);
-    
+
             // sync to the todoNode
             this.clickedTodo.textContent = e.target.value;
-        })
-        note.addEventListener('input', e => {
+        });
+        note.addEventListener('input', (e) => {
             this.todoObj.setNote(e.target.value);
-        })
+        });
     }
-    
+
     displayDatePicker() {
-
-        import (/* webpackPrefetch: true */ './date-picker').then(module => {
-
+        import(/* webpackPrefetch: true */ './date-picker').then((module) => {
             const DatePicker = module.default;
-        
+
             // created datePicker Obj
             const datePicker = new DatePicker(this.todoObj);
-
         });
     }
 
     displaypriorityPanel() {
+        import(/* webpackPrefetch: true */ './priority-picker').then(
+            (module) => {
+                const Priority = module.default;
 
-        import (/* webpackPrefetch: true */ './priority-picker').then(module => {
-
-            const Priority = module.default;
-        
-            // create priority panel Obj (todos, index, priority, clickedTodo)
-            const priorityPanel = new Priority(this.todoObj, this.priority, this.clickedTodo);
-
-        });
+                // create priority panel Obj (todos, index, priority, clickedTodo)
+                const priorityPanel = new Priority(
+                    this.todoObj,
+                    this.clickedTodo
+                );
+            }
+        );
     }
 
     deleteTodoClicked() {
-    
         // delete the todo object from array
         deleteTodo(this.todoObj.getTitle(), this.todoObj.getId());
-    
+
         // close the dialog
         this.dialog.close();
-        
+
         // delete the todo from the DOM
         this.clickedTodo.parentElement.remove();
-    
-        // fix the data-index of the following todo-items
-        this.fixTodoIndices();
     }
 
-    fixTodoIndices() {
-
-        const allTodoBtns = document.querySelectorAll(`div.list button[data-todo-index]`);
-    
-        allTodoBtns.forEach(btn => {
-            console.log(btn.dataset.todoIndex);
-            if(Number(btn.dataset.todoIndex) >= Number(this.index))
-                btn.dataset.todoIndex -= 1;
-        });
-    }
-
-    cancelDialogClicked()  {
-    
+    cancelDialogClicked() {
         this.dialog.close();
-    
+
         // close priority changing panel if not closed
         this.divPriorities.classList.remove('visible');
 
         // close date-picker panel
         this.divDatePicker.classList.remove('visible');
     }
-     
 }
